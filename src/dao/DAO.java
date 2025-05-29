e78760c2aebc4840e23bfea21745582aa315182d
n_waseda <n_waseda@LAPTOP-CI75NEBC>
2025-05-29 12:05:21 +0900

    5/29 エラー修繕

--- src/dao/DAO.java
+++ src/dao/DAO.java
@@ -11,7 +11,7 @@ public class DAO {
 	public Connection getConnection() throws Exception {
 		if (ds==null) {
 			InitialContext ic=new InitialContext();
-			ds=(DataSource)ic.lookup("java:/comp/env/jdbc/book");
+			ds=(DataSource)ic.lookup("java:/comp/env/jdbc/kokushimusou");
 		}
 		return ds.getConnection();
 
